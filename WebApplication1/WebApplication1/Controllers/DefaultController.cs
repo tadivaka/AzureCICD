18e0c829becba8c43b2b45df50225f7ec6019bcc
TadivakaV <TadivakaV@KIHYDL0436>
2018-05-07 13:11:31 +0530

    second check in.

---
+++ WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewData["sourceName"] = ConfigurationManager.AppSettings["SourceName"].ToString();
+            return View();
+        }
+    }
+}
